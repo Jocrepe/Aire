2095a6121df10362575170082d61c2b29855b223
Krittapol Nammungkun <krittapolcrepe@outlook.com>
2026-01-06 01:59:42 +0700

    fix

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,10 +3,10 @@ dotenv.config()
 
 import app from './app.js'
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log(`server is running at http://localhost:${PORT}`)
+  console.log(`server is running at ${PORT}`)
 })
 
 
