4cf811f5d1b1cb12d0acd844acb1810962438804
Krittapol Nammungkun <krittapolcrepe@outlook.com>
2026-01-06 04:04:51 +0700

    fix eoeo

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,4 +1,5 @@
 import '@/style.css'
+import '/swiper/css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,15 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from 'tailwindcss'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
   ],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(),
+        require('autoprefixer'),
+      ],
+    },
+  },
+  optimizeDeps: {
+    exclude: ['swiper'],
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
